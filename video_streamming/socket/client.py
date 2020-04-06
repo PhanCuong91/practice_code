0d14cc61fb2f00b51fa768427673430226b45e5d
Phan Cuong (uib76414) <cuong.phan-ext@continental-corporation.com>
2020-04-06 22:32:53 +0800

    basic chat room

--- video_streamming/socket/client.py
+++ video_streamming/socket/client.py
@@ -1,36 +1,52 @@
 import socket
-import os, sys, errno
+import sys
+import errno
 from threading import Thread
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-s.setblocking(False)
-HEADERSIZE = 10
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((socket.gethostname(), 1234))
+client.setblocking(False)
+HEADER_SIZE = 10
 
 
-def send_mess(soc):
-
+def send_mess(client, user_name):
+    """
+    send mess to chat room
+    :param client: client connection
+    :param user_name: user name
+    :return:
+    """
     while True:
         try:
-            mess = input(f'{user_name} > ')
-            mess = f"{user_name} >> {mess}"
-            mess_header = f"{len(mess):<{HEADERSIZE}}"
-            soc.send(bytes(mess_header+mess, "utf-8"))
+            print(f"{user_name} >>")
+            mess = input(f'')
+            client.send(bytes(encode_with_header(mess), "utf-8"))
+            # send quit mess , then break while loop
+            if mess == 'quit()':
+                break
         except Exception as e:
             print(e)
 
 
 def receive_mess(soc):
-
+    """
+    receive mess from server
+    :param soc: client connection
+    :return: None
+    """
     while True:
         try:
-            mess_header_size = soc.recv(HEADERSIZE)
+            mess_header_size = soc.recv(HEADER_SIZE)
             mess_len = int(mess_header_size.decode('utf-8'))
             mess = soc.recv(mess_len).decode('utf-8')
+            # receive mess from server before server close the connection
             print(f"{mess}")
+            if mess == 'Close connect':
+                break
         except IOError as e:
             # This is normal on non blocking connections - when there are no incoming data error is going to be raised
             # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-            # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+            # We are going to check for both - if one of them - that's expected,
+            # means no incoming data, continue as normal
             # If we got different error code - something happened
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
                 print('Reading error: {}'.format(str(e)))
@@ -39,8 +55,21 @@ def receive_mess(soc):
             continue
 
 
+def encode_with_header(mess):
+    return f"{len(mess):<{HEADER_SIZE}}" + mess
+
+
+def get_mess_len(conn):
+    mess_header_size = conn.recv(HEADER_SIZE)
+    return int(mess_header_size.decode('utf-8'))
+
+
 if __name__ == "__main__":
-    user_name = input("user name is ")
-    Thread(target=send_mess, args=(s,)).start()
-    Thread(target=receive_mess, args=(s,)).start()
+    print("your username is:")
+    user_name = input("")
+    mess = f"{user_name}"
+    mess_header = f"{len(mess):<{HEADER_SIZE}}"
+    client.send(bytes(mess_header + mess, "utf-8"))
+    Thread(target=send_mess, args=(client, user_name)).start()
+    Thread(target=receive_mess, args=(client,)).start()
 
--- video_streamming/socket/server.py
+++ video_streamming/socket/server.py
@@ -1,83 +1,132 @@
 import socket
-import time
 from threading import Thread
-HEADERSIZE = 10
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((socket.gethostname(), 1234))
-# s.listen(1)
+
+HEADER_SIZE = 10
 global host
 global port
 host = socket.gethostname()
 port = 1234
-
 arr_conn = []
-arr_add = []
 
 
 def create_socket():
+    """
+    create a socket for server
+    :return: None
+    """
     try:
-        global soc
-        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        global server
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print(f"Socket creation with {host} is successful")
     except socket.error as msg:
         print(f"Socket creation error: {msg}")
 
 
 def bind_socket():
+    """
+    Binding socket for server
+    :return:
+    """
     try:
-        soc.bind((host, port))
+        server.bind((host, port))
         print(f"Socket binding port {port} is successful")
-        soc.listen(5)
+        server.listen(5)
     except socket.error as msg:
         print(f"Socket binding error: {msg}")
 
 
-def accept_socket(arr_con, arr_address):
-    conn, add = soc.accept()
+def accept_socket():
+    """
+    accept new client
+    broadcast new user name to chat room
+    run receive message function
+    :return:
+    """
+    conn, add = server.accept()
+    # add new connection to array which use for broadcasting
     arr_conn.append(conn)
-    print(f"connection to is accepted with {conn} and {add}")
-    Thread(target=receive_mess, args=(conn,)).start()
+    # get user name and broadcast to other user name who was in chat room
+    mess_len = get_mess_len(conn)
+    username = conn.recv(mess_len).decode('utf-8')
+    print(f"connection: {username} joined chat room")
+    broadcast(conn, f"connection: {username} joined chat room")
+    # run receive_mess thread
+    Thread(target=receive_mess, args=(conn, username)).start()
 
 
 def broadcast(except_client, mess):
+    """
+    broadcast message to other clients except itself
+    :param except_client: itself client
+    :param mess: message
+    :return: None
+    """
+
     for client in arr_conn:
+        # if client is not excepted client
+        # then send message
         if except_client != client:
             try:
-                mess = f"{len(mess):<{HEADERSIZE}}" + mess
-                client.send(bytes(mess, "utf-8"))
+                mess = f"{len(mess):<{HEADER_SIZE}}" + mess
+                send_mess(client, mess)
             except Exception as e:
                 print(f"Broadcast exception is {e}")
 
 
-def receive_mess(conn):
+def receive_mess(conn, username):
+    """
+    get message from client and broadcast to other client
+    :param conn: connection of client
+    :param username: user name of client
+    :return: None
+    """
     while True:
         try:
-            mess_header_size = conn.recv(HEADERSIZE)
-            mess_len = int(mess_header_size.decode('utf-8'))
+            mess_len = get_mess_len(conn)
             mess = conn.recv(mess_len).decode('utf-8')
-            print(f"{mess}")
-            if mess == 'Use quit() or Ctrl-Z plus Return to exit':
+            print(f"{username} >> {mess}")
+            # if server receives quit message
+            # broadcast respective user left chat room and close connection
+            if mess == 'quit()':
+                # then server send "Close connect" mess to respective client,
+                mess = f"Close connect"
+                mess = encode_with_header(mess)
+                send_mess(conn, mess)
+                # broadcast respective user left chat room
+                broadcast(conn, f"{username} left chat room")
+                # close connection and remove connection from connection array
                 conn.close()
                 arr_conn.remove(conn)
-                print(f"Disconnection")
+                print(f"Disconnection: {username} quited chat room")
                 break
+            # broadcast message with respect to user name
+            mess = f"{username} >> {mess}"
             broadcast(conn, mess)
         except Exception as e:
             print(f"[Receive message] Exception is {e}")
-    # conn.close()
 
 
-def send_command(conn):
-    print(f"Connection from {conn} has been establish!")
-    mess = f"welcome to socket and time is "
-    mess = f"{len(mess):<{HEADERSIZE}}" + mess
+def send_mess(conn, mess):
+    """
+    send mess
+    :param conn:  connection
+    :param mess: message
+    :return: None
+    """
     conn.send(bytes(mess, "utf-8"))
-    conn.close()
+
+
+def encode_with_header(mess):
+    return f"{len(mess):<{HEADER_SIZE}}" + mess
+
+
+def get_mess_len(conn):
+    mess_header_size = conn.recv(HEADER_SIZE)
+    return int(mess_header_size.decode('utf-8'))
 
 
 if __name__ == "__main__":
     create_socket()
     bind_socket()
     while True:
-        accept_socket(arr_conn, arr_add)
-        # send_command()
+        accept_socket()
